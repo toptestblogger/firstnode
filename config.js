ae478c252ffb408b60ea0664abf3b7d360bd067c
unknown <toptestblogger@gmail.com>
2017-06-01 20:17:21 +0530

    kalyan

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config={
 	database:{
-		mode:'local', // can be local or remote
-		host:'localhost',
+		mode:'remote', // can be local or remote
+		host:'32-1a.mongo.evennode.com:27017,32-1b.mongo.evennode.com:27017',
 		port:27017,
-		dbname:'nodetest1',
-		username:'kalyan',
+		dbname:'3f26aec503398ebede359a4846cb24e7',
+		username:'3f26aec503398ebede359a4846cb24e7',
 		password:'kalyan'
 	}
 };
