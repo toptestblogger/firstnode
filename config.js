cfecbace75a02a3cd05f001b41d6615badea5867
toptestblogger <toptestblogger@gmail.com>
2017-06-02 09:22:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config={
 	database:{
 		mode:'remote', // can be local or remote
-		host:'32-1a.mongo.evennode.com:27017,32-1b.mongo.evennode.com:27017',
+		host:'_env(MONGODB_URL_INT):27017',
 		port:27017,
-		dbname:'3f26aec503398ebede359a4846cb24e7',
-		username:'3f26aec503398ebede359a4846cb24e7',
-		password:'kalyan'
+		dbname:'teen_production',
+		username:'',
+		password:''
 	}
 };
 
